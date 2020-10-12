ab2ef6bbc01bac592cb09d5c5f02a571013a8401
Lucas Olivier <lucas.olivierfs@gmail.com>
2020-10-12 09:04:26 -0300

    Primeiro commit, criação de projeto

---
+++ src/test/java/com/petshopapp/petshopapi/PetshopApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.petshopapp.petshopapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetshopApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
