740a4eef072466d195f0137eea20aa0e81820ce3
Lucas Olivier <lucas.olivierfs@gmail.com>
2020-10-13 17:55:52 -0300

    Adicionando userDTO

---
+++ src/main/java/com/petshopapp/petshopapi/dto/UserDTO.java
@@ -0,0 +1,51 @@
+package com.petshopapp.petshopapi.dto;
+
+import java.io.Serializable;
+
+import com.petshopapp.petshopapi.domain.User;
+
+public class UserDTO implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	private String id;
+
+	private String name;
+	private String email;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	UserDTO() {
+	}
+
+	public UserDTO(User user) {
+
+		this.id = user.getId();
+		this.name = user.getName();
+		this.email = user.getEmail();
+
+	}
+
+}
--- src/main/java/com/petshopapp/petshopapi/resources/UserResource.java
+++ src/main/java/com/petshopapp/petshopapi/resources/UserResource.java
@@ -1,8 +1,7 @@
 package com.petshopapp.petshopapi.resources;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -11,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.petshopapp.petshopapi.domain.User;
+import com.petshopapp.petshopapi.dto.UserDTO;
 import com.petshopapp.petshopapi.services.UserService;
 
 @RestController
@@ -21,9 +21,10 @@ public class UserResource {
 	private UserService service;
 
 	@RequestMapping(method = RequestMethod.GET)
-	public ResponseEntity<List<User>> findAll() {
+	public ResponseEntity<List<UserDTO>> findAll() {
 		List<User> users = service.findAll();
-		return ResponseEntity.ok().body(users);
+		List<UserDTO> usersDTO = users.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(usersDTO);
 	}
 
 }
